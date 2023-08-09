772722f8e67300000b5c5b2851338a12cae0a64c
Vladislav Melnik <vladislav.melnik@protonmail.com>
2023-08-09 20:49:26 +0200

    improve interface

--- bootstrap-sandbox/src/main.rs
+++ bootstrap-sandbox/src/main.rs
@@ -9,8 +9,24 @@ mod replay;
 
 use std::{env, path::PathBuf};
 
+use libp2p::Multiaddr;
 use libp2p_rpc_behaviour::BehaviourBuilder;
 use structopt::StructOpt;
+use mina_transport::ed25519::SecretKey;
+
+#[derive(StructOpt)]
+struct Args {
+    #[structopt(long, default_value = "target/default")]
+    path: PathBuf,
+    #[structopt(long, default_value = "667b328bfc09ced12191d099f234575b006b6b193f5441a6fa744feacd9744db")]
+    chain_id: String,
+    #[structopt(long)]
+    listen: Vec<Multiaddr>,
+    #[structopt(long)]
+    peer: Vec<Multiaddr>,
+    #[structopt(subcommand)]
+    cmd: Command,
+}
 
 #[derive(StructOpt)]
 enum Command {
@@ -34,43 +50,44 @@ enum Command {
 async fn main() {
     env_logger::init();
 
-    let path = env::var("MINA_RECORD_PATH")
-        .map(PathBuf::from)
-        .unwrap_or_else(|_| PathBuf::from("target/record"));
+    let Args { path, chain_id, listen, peer, cmd } = Args::from_args();
 
-    let local_key: libp2p::identity::Keypair = {
-        let mut sk_bytes = *b"\
-            \x34\x85\xA7\x4E\x47\x34\xFE\x48\x0E\xFA\xFF\x38\x99\x26\xD6\x19\
-            \xAC\xC5\x1A\xFC\x1D\xDB\x96\x29\x4B\x5E\xC2\x5D\x15\x7B\x54\x5D\
-        ";
-        let sk = mina_transport::ed25519::SecretKey::from_bytes(&mut sk_bytes).unwrap();
-        mina_transport::ed25519::Keypair::from(sk).into()
-    };
+    let sk = env::var("OPENMINA_P2P_SEC_KEY")
+        .map(|key| {
+            let mut bytes = bs58::decode(key).with_check(Some(0x80)).into_vec().unwrap();
+            SecretKey::from_bytes(&mut bytes[1..]).unwrap()
+        })
+        .unwrap_or_else(|_| {
+            let mut bytes = rand::random::<[u8; 32]>();
+            log::info!("{}", bs58::encode(&bytes).with_check_version(0x80).into_string());
+            let sk = SecretKey::from_bytes(&mut bytes).unwrap();
+            sk
+        });
+
+    let local_key: libp2p::identity::Keypair = mina_transport::ed25519::Keypair::from(sk).into();
     log::info!("{}", local_key.public().to_peer_id());
 
-    let chain_id = b"667b328bfc09ced12191d099f234575b006b6b193f5441a6fa744feacd9744db";
-    let listen_on = [
-        "/ip6/::/tcp/8302".parse().unwrap(),
-        "/ip4/0.0.0.0/tcp/8302".parse().unwrap(),
-    ];
-    let peers = [
-        // "/ip4/135.181.217.23/tcp/30737/p2p/12D3KooWAVvZjW5m5LmhJrCUq2VtvG3drAsWxewMobgoUpewtqcp",
-        // "/ip4/35.192.28.217/tcp/10000/p2p/12D3KooWAdgYL6hv18M3iDBdaK1dRygPivSfAfBNDzie6YqydVbs",
-        "/ip4/34.170.114.52/tcp/10001/p2p/12D3KooWLjs54xHzVmMmGYb7W5RVibqbwD1co7M2ZMfPgPm7iAag",
-        // "/ip4/34.123.4.144/tcp/10002/p2p/12D3KooWEiGVAFC7curXWXiGZyMWnZK9h8BKr88U8D5PKV3dXciv",
-    ]
-    .into_iter()
-    .map(|x| x.parse())
-    .flatten();
-    // /dns4/seed-1.berkeley.o1test.net
+    // let listen_on = [
+    //     "/ip6/::/tcp/8302".parse().unwrap(),
+    //     "/ip4/0.0.0.0/tcp/8302".parse().unwrap(),
+    // ];
+    // let peers = [
+    //     "/ip4/35.192.28.217/tcp/10000/p2p/12D3KooWAdgYL6hv18M3iDBdaK1dRygPivSfAfBNDzie6YqydVbs",
+    //     "/ip4/34.170.114.52/tcp/10001/p2p/12D3KooWLjs54xHzVmMmGYb7W5RVibqbwD1co7M2ZMfPgPm7iAag",
+    //     "/ip4/34.123.4.144/tcp/10002/p2p/12D3KooWEiGVAFC7curXWXiGZyMWnZK9h8BKr88U8D5PKV3dXciv",
+    //     "/ip4/127.0.0.1/tcp/8303/p2p/12D3KooWP9mwZjBdyrr2rDMKWDMo2vdLpajDobXLeyYaQtJUg8NT",
+    // ]
+    // .into_iter()
+    // .map(|x| x.parse())
+    // .flatten();
 
-    match Command::from_args() {
+    match cmd {
         Command::Again { height } => {
             bootstrap::again(&path, height).await;
         }
         Command::Record { bootstrap } => {
             let behaviour = BehaviourBuilder::default().build();
-            let swarm = mina_transport::swarm(local_key, chain_id, listen_on, peers, behaviour);
+            let swarm = mina_transport::swarm(local_key, chain_id.as_bytes(), listen, peer, behaviour);
 
             record::run(swarm, &path, bootstrap).await
         }
@@ -88,7 +105,7 @@ async fn main() {
                 .register_method::<GetTransitionChainV2>()
                 .register_method::<GetTransitionChainProofV1ForV2>()
                 .build();
-            let swarm = mina_transport::swarm(local_key, chain_id, listen_on, [], behaviour);
+            let swarm = mina_transport::swarm(local_key, chain_id.as_bytes(), listen, [], behaviour);
 
             replay::run(swarm, &path, height).await
         }
